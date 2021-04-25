0ad331d7f71f5e514c99f60dab308c7e96f823ab
ParejaOnPurpose <pamm_jackson@yahoo.com>
2021-04-25 19:52:15 -0400

    Unfinished Todolist review app server js file.

--- server.js
+++ server.js
@@ -4,18 +4,38 @@ const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 
 let db,
-    dbConnectionStr = 'mongodb+srv://ParejaOnPurpose:ParejaOnPurpose@cluster0.8osuw.mongodb.net/
-    todo?retryWrites=true&w=majority',
+    dbConnectionStr = 'mongodb+srv://ParejaOnPurpose:ParejaOnPurpose@cluster0.8osuw.mongodb.net/todo?retryWrites=true&w=majority',
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionsStr, {useUnifiedTopology: true})
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
   .then(client => {
     console.log(`Hey, connected to ${dbName} database`)
     db = client.db(dbName)
   })
-.catch (err)=> {
+.catch(err => {
   console.log(err)
 })
-  app.listen(Port, ()=>{
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get('/',(req,res) =>{
+    db.collection('todos').find().toArray()
+    .then(data =>{
+        res.render('index.ejs',{zebra:data})
+    })
+
+app.post('/createToDo', (req,res) => {
+  db.collection('todos').insertOne ({todo: req.body.todoItem, completed: false})
+    .then(result =>{
+        console.log('Todos has been added!')
+        res.redirect('/')
+    })
+
+  })
+
+  app.listen(PORT, ()=>{
       console.log('Server is running, you better catch it.')
   })
